42c028fc8699a17c4e8423400c6e2cfdd94c466f
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-05-02 23:12:46 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+long_description = open("README.rst").read()
+
+setup(
+    name="Cast Extensions",
+    version="0.1",
+    license="MIT",
+    url="https://github.com/Eerovil/cast-extensions.git",
+    author="Eero Vilpponen",
+    author_email="eero@vilpponen.fi",
+    description="Cast Extensions",
+    long_description=long_description,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=list(val.strip() for val in open("pip-requirements-adb.txt")),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
