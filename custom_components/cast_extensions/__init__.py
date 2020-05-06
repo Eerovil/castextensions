eb3e676c34aa7f79fc8befde81cc4f345403d06d
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-05-06 13:45:16 +0300

    Use signals to access chromecast device

--- custom_components/cast_extensions/__init__.py
+++ custom_components/cast_extensions/__init__.py
@@ -1,12 +1,8 @@
 """Component to add new Cast Applications support."""
 
 import traceback
-import os
-import appdirs
 import json
 import logging
-import functools as ft
-from pychromecast import get_chromecasts
 
 from homeassistant import config_entries
 import homeassistant.helpers.config_validation as cv
@@ -22,7 +18,7 @@ from .app_yleareena import YleAreena
 from .app_netflix import Netflix
 from .app_supla import find_supla_program
 
-from .util_chromecast import Chromecast
+from .util_chromecast import ChromecastWrapper
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -40,7 +36,7 @@ async def async_setup_entry(hass, config_entry: config_entries.ConfigEntry):
 
         for entry in registry.entities.values():
             if entry.entity_id == call.data["entity_id"][0]:
-                chromecast_name = entry.original_name
+                entity = entry
                 break
         else:
             _LOGGER.error('Entity %s not found', call.data["entity_id"][0])
@@ -49,9 +45,7 @@ async def async_setup_entry(hass, config_entry: config_entries.ConfigEntry):
         app_data = json.loads(call.data["media_content_id"])
         app_name = app_data.pop('app_name')
 
-        await hass.async_add_job(
-            ft.partial(quick_play, chromecast_name, app_name, app_data, config=config_entry)
-        )
+        quick_play(hass, entity, app_name, app_data, config=config_entry)
 
     hass.helpers.service.async_register_admin_service(
         DOMAIN,
@@ -62,22 +56,20 @@ async def async_setup_entry(hass, config_entry: config_entries.ConfigEntry):
     return True
 
 
-def quick_play(chromecast_name, app_name, data, config: config_entries.ConfigEntry):
+def quick_play(hass, entity, app_name, data, config: config_entries.ConfigEntry):
+    service_data = {}
 
-    from pychromecast.controllers.media import MediaController
-    from pychromecast.controllers.yleareena import YleAreenaController
-    from pychromecast.controllers.supla import SuplaController
+    cast_wrapper = ChromecastWrapper(hass, entity)
 
-    kwargs = {}
-
-    cast = Chromecast([cast for cast in get_chromecasts() if cast.device.friendly_name == chromecast_name][0])
+    _LOGGER.info("Starting quick_play of app %s for chromecast %s",
+                 app_name, cast_wrapper.get_name())
 
     if app_name == 'dlna':
-        controller = MediaController()
-        kwargs = {
-            'media_url': find_dlna(data.pop('dlna_server'), data.pop('media_id'),
-                                   index=data.pop('index', None)),
-            'content_type': data.pop('media_type', None),
+        app_name = 'media'
+        service_data = {
+            'media_id': find_dlna(data.pop('dlna_server'), data.pop('media_id'),
+                                  index=data.pop('index', None)),
+            'media_type': data.pop('media_type', 'video/mp4'),
         }
     elif app_name == 'yleareena':
         program_id = data.pop('media_id')
@@ -89,11 +81,10 @@ def quick_play(chromecast_name, app_name, data, config: config_entries.ConfigEnt
             else:
                 program_id = areena.get_series_latest_id(program_id)
         kaltura_id = YleAreena.get_kaltura_id(program_id)
-        controller = YleAreenaController()
-        kwargs = {
-            'kaltura_id': kaltura_id,
-            'audio_language': data.pop('audio_lang', ''),
-            'text_language': data.pop('text_lang', 'off'),
+        service_data = {
+            'media_id': kaltura_id,
+            'audio_lang': data.pop('audio_lang', ''),
+            'text_lang': data.pop('text_lang', 'off'),
         }
     elif app_name == 'supla':
         media_id = data.pop('media_id')
@@ -101,39 +92,29 @@ def quick_play(chromecast_name, app_name, data, config: config_entries.ConfigEnt
         if data.pop('media_type', None) == 'program':
             media_id = find_supla_program(media_id, match=data.pop('title_match', None))
 
-        controller = SuplaController()
-        kwargs = {
+        service_data = {
             'media_id': media_id,
-            'is_live': data.pop('is_live', None),
+            'is_live': data.pop('is_live', False),
         }
 
     # *** Start Special apps not using pychromecast ***
     elif app_name == 'netflix':
-        if cast.running_app == "netflix":
-            # TODO: Async needed here. If netflix is running, it needs to be stopped. But
-            # chromecast is really slow to first stop an app, then start another one (takes up to
-            # 10 seconds). Blocking for 10 seconds here is not something we want, so instead
-            # just "quit" netflix by running an empty file.
-            cast.play_media("http://localhost")
-        else:
-            # Start the netflix app, just for show (otherwise chromecast dashboard would load here
-            # while we wait: Bad UI)
-            cast.start_app("netflix")
+        cast_wrapper.quit()
+        cast_wrapper.start_app("netflix")
         try:
             netflix = Netflix(
-                cast.get_name(), connect_ip=config.data.get("adb_connect", None)
+                entity.original_name, connect_ip=config.data.get("adb_connect", None)
             )
             netflix.main(data.pop('media_id'))
         except Exception:
             traceback.print_exc()
-            cast.quit()
+            cast_wrapper.quit()
     # *** End Special apps not using pychromecast ***
     else:
         raise NotImplementedError()
 
-    if kwargs:
-        cast.register_handler(controller)
-        controller.quick_play(**kwargs)
+    if service_data:
+        cast_wrapper.quick_play(app_name, service_data)
 
     if data:
-        controller.logger.warning('Unused data in quick_play: %s', data)
+        _LOGGER.warning('Unused data in quick_play: %s', data)
